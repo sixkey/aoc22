e7f475512a4bc71181dcf4db395914aad90ecc5c
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-02 00:20:15 +0100

    day01: tree racket solution

--- day01/aoc01.rkt
+++ day01/aoc01.rkt
@@ -1,26 +1,22 @@
-#lang racket
+                                  #lang racket
 
-( define ( on-file filename f )
-    ( f ( port->string ( open-input-file filename ) #:close? #t ) ) 
-)
+                        ( define ( on-file filename f )
+        ( f ( port->string ( open-input-file filename ) #:close? #t ) ) )
 
-( define ( parse-input str ) 
-    ( map ( lambda ( e ) ( map string->number ( string-split e "\n" ) ) ) ( string-split str "\n\n" ) )
-) 
+                          ( define ( parse-input str ) 
+     ( map ( lambda ( e ) ( map string->number ( string-split e "\n" ) ) ) 
+                         ( string-split str "\n\n" ) ) ) 
 
-( define ( on-input filename f ) 
-    ( on-file filename ( lambda ( c ) ( f ( parse-input c ) ) ) )
-)
+                        ( define ( on-input filename f ) 
+         ( on-file filename ( lambda ( c ) ( f ( parse-input c ) ) ) ) )
 
-( define ( sum l ) ( foldr + 0 l ) )
+                      ( define ( sum l ) ( foldr + 0 l ) )
 
-( define ( part-1 cont ) 
-    ( sum ( take ( sort ( map sum cont ) > ) 1 ) )
-)
+                            ( define ( part-1 cont ) 
+                 ( sum ( take ( sort ( map sum cont ) > ) 1 ) ) )
 
-( define ( part-2 cont ) 
-    ( sum ( take ( sort ( map sum cont ) > ) 3 ) )
-)
+                            ( define ( part-2 cont ) 
+                 ( sum ( take ( sort ( map sum cont ) > ) 3 ) ) )
 
-( on-input "i.aoc" part-1 ) 
-( on-input "i.aoc" part-2 ) 
+                          ( on-input "i.aoc" part-1 ) 
+                          ( on-input "i.aoc" part-2 ) 
