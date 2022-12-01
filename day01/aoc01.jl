d9dcb322c9cad6270604b39f7de611cb01e0139a
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-01 09:50:42 +0100

    day01: make tree

--- day01/aoc01.jl
+++ day01/aoc01.jl
@@ -1,23 +1,22 @@
-function load_input( filename )
-    sums = []
-    open( filename ) do file
-        lines = readlines( filename )
-        loc = []
-        for line in lines
-            if line == ""
-                push!( sums, loc )
-                loc = []
-            else
-                push!( loc, parse( Int, line ) )
-            end
-        end
-    end
-    return sums
-end
+                        function load_input( filename )
+                                   sums = []
+                            open( filename ) do file
+                                    loc = []
+                         for line in readlines( file )
+                                 if line == ""
+                               push!( sums, loc )
+                                    loc = []
+                                      else
+                        push!( loc, parse( Int, line ) )
+                                      end
+                                      end
+                                      end
+                                  return sums
+                                      end
 
-part1( vec ) = maximum( sum.( vec ) )
-part2( vec ) = sum( first( sort( sum.( vec ), rev = true ), 3 ) )
+                     part1( vec ) = maximum( sum.( vec ) )
+       part2( vec ) = sum( first( sort( sum.( vec ), rev = true ), 3 ) )
 
-vec = load_input( "i.aoc" )
-print( "part1: ", part1( vec ), "\n" )
-print( "part2: ", part2( vec ), "\n" )
+                          vec = load_input( "i.aoc" )
+                     print( "part1: ", part1( vec ), "\n" )
+                     print( "part2: ", part2( vec ), "\n" )
