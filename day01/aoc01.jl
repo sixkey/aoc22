0aea02fc6f70d6beb39619b9a8415183b926b901
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-01 09:43:00 +0100

    day1: initial solution

---
+++ day01/aoc01.jl
@@ -0,0 +1,23 @@
+function load_input( filename )
+    sums = []
+    open( filename ) do file
+        lines = readlines( filename )
+        loc = []
+        for line in lines
+            if line == ""
+                push!( sums, loc )
+                loc = []
+            else
+                push!( loc, parse( Int, line ) )
+            end
+        end
+    end
+    return sums
+end
+
+part1( vec ) = maximum( sum.( vec ) )
+part2( vec ) = sum( first( sort( sum.( vec ), rev = true ), 3 ) )
+
+vec = load_input( "i.aoc" )
+print( "part1: ", part1( vec ), "\n" )
+print( "part2: ", part2( vec ), "\n" )
