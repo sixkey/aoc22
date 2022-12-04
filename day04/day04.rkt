e40551e01b5bb7ae9a86448863da821ee2f446d5
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-04 14:02:26 +0100

    day04: center racket solution

--- day04/day04.rkt
+++ day04/day04.rkt
@@ -1,32 +1,32 @@
-#lang racket
+                                  #lang racket
 
-( require parsack )
-( require predicates )
+                              ( require parsack )
+                             ( require predicates )
 
-( require "../kck/kck.rkt" )
+                          ( require "../kck/kck.rkt" )
 
-( define ( parse line )
-  ( list->pair
-  ( map ( lambda ( l ) ( list->pair ( map string->number ( string-split l "-" ) ) ) )
-        ( string-split line "," ) ) )
-)
+                            ( define ( parse line )
+                                  ( list->pair
+( map ( lambda ( l ) ( list->pair ( map string->number ( string-split l "-" ) ) ) )
+                         ( string-split line "," ) ) )
+                                       )
 
-( define ( contains a b )
-  ( and ( >= ( car b ) ( car a ) )
-        ( <= ( cdr b ) ( cdr a ) ) ) )
+                           ( define ( contains a b )
+                        ( and ( >= ( car b ) ( car a ) )
+                         ( <= ( cdr b ) ( cdr a ) ) ) )
 
-( define ( overlap a b )
-  ( if ( > ( car a ) ( car b ) )
-       ( overlap b a )
-       ( >= ( cdr a ) ( car b ) ) ) )
+                            ( define ( overlap a b )
+                         ( if ( > ( car a ) ( car b ) )
+                                ( overlap b a )
+                         ( >= ( cdr a ) ( car b ) ) ) )
 
-( define ( part-1 content )
-    ( count ( ind-pair ( lambda ( a b )
-                                ( or ( contains a b ) ( contains b a ) ) ) )
-              content ) )
+                          ( define ( part-1 content )
+                      ( count ( ind-pair ( lambda ( a b )
+                  ( or ( contains a b ) ( contains b a ) ) ) )
+                                  content ) )
 
-( define ( part-2 content )
-    ( count ( ind-pair overlap ) content ) )
+                          ( define ( part-2 content )
+                    ( count ( ind-pair overlap ) content ) )
 
-( call-with-lines "i.aoc" part-1 parse )
-( call-with-lines "i.aoc" part-2 parse )
+                    ( call-with-lines "i.aoc" part-1 parse )
+                    ( call-with-lines "i.aoc" part-2 parse )
