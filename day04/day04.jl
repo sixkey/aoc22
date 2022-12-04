bcef0b3dc3223885a649674de2c2d8f9156ebc32
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-04 14:54:43 +0100

    day04: tree julia version

--- day04/day04.jl
+++ day04/day04.jl
@@ -1,28 +1,28 @@
-splitby( b ) = x -> split( x, b )
-torange( x ) = range( x[1], stop=x[2] )
-int = x -> parse( Int, x )
-broadby( f ) = x -> broadcast( f, x )
-tuple_of_list( l ) = ( l[ 1 ], l[ 2 ] )
-ind_pair( f ) = p -> f( p[ 1 ], p[ 2 ] )
+                       splitby( b ) = x -> split( x, b )
+                    torange( x ) = range( x[1], stop=x[2] )
+                           int = x -> parse( Int, x )
+                     broadby( f ) = x -> broadcast( f, x )
+                    tuple_of_list( l ) = ( l[ 1 ], l[ 2 ] )
+                    ind_pair( f ) = p -> f( p[ 1 ], p[ 2 ] )
 
-function on_input( solver, filename )
-    open( filename ) do f
-        eachline( f ) .|> ( x -> split( x, "," ) .|> splitby( "-" ) .|> broadby( int ) .|> torange ) .|>
-                          tuple_of_list |> solver
-    end
-end
+                     function on_input( solver, filename )
+                             open( filename ) do f
+eachline( f ) .|> ( x -> split( x, "," ) .|> splitby( "-" ) .|> broadby( int ) .|> torange ) .|>
+                            tuple_of_list |> solver
+                                      end
+                                      end
 
-function part_1( filename )
-    on_input( filename ) do content
-        content .|> ind_pair( ( a, b ) -> issubset( a, b ) || issubset( b, a ) ) |> sum
-    end
-end
+                          function part_1( filename )
+                        on_input( filename ) do content
+content .|> ind_pair( ( a, b ) -> issubset( a, b ) || issubset( b, a ) ) |> sum
+                                      end
+                                      end
 
-function part_2( filename )
-    on_input( filename ) do content
-        content .|> ind_pair( intersect ) .|> isempty .|> ( x -> !x ) |> sum
-    end
-end
+                          function part_2( filename )
+                        on_input( filename ) do content
+      content .|> ind_pair( intersect ) .|> isempty .|> ( x -> !x ) |> sum
+                                      end
+                                      end
 
-println( part_1( "i.aoc" ) )
-println( part_2( "i.aoc" ) )
+                          println( part_1( "i.aoc" ) )
+                          println( part_2( "i.aoc" ) )
