f54ce875de775e6bfc63ac576621a11356ceae78
Filip Kučerák <xkucerak@fi.muni.cz>
2022-12-06 14:20:56 +0100

    day06: racket tree solution

--- day06/day06.rkt
+++ day06/day06.rkt
@@ -1,11 +1,14 @@
-#lang racket
-
-( require "../kck/kck.rkt" algorithms )
-
-( define ( solver window )
-  ( lambda ( content )
-    ( + ( index-where ( sliding ( string->list content ) window ) all-unique ) window ) )
-)
-
-( call-with-content "i.aoc" ( solver 4 ) string-trim )
-( call-with-content "i.aoc" ( solver 14 ) string-trim )
+                                  #lang racket
+                                   ( require 
+                                "../kck/kck.rkt" 
+                             algorithms ) ( define 
+                                    ( solver 
+                               window ) ( lambda 
+                         ( content ) ( + ( index-where 
+                        ( sliding ( string->list content 
+                      ) window ) all-unique ) window ) ) )
+                    ( call-with-content "i.aoc" ( solver 4 ) 
+                                string-trim ) ( 
+                               call-with-content 
+                                "i.aoc" ( solver 
+                               14 ) string-trim )
